0607377dbe3f397d557b2d0d52008d41ce725849
Youssef Mahmoud <139473311+ysseff@users.noreply.github.com>
2024-12-08 23:29:54 +0200

    updated conversationResponse

--- MyChatAppBackend/Contracts/ConversationResponse.cs
+++ MyChatAppBackend/Contracts/ConversationResponse.cs
@@ -2,9 +2,7 @@ namespace MyChatAppBackend.Contracts;
 
 public class ConversationResponse
 {
-    public int ConversationId { get; set; }
-    public string User1Id { get; set; }
-    public string User1Username { get; set; }
-    public string User2Id { get; set; }
-    public string User2Username { get; set; }
+    public int ConversationId { get; set; } // The ID of the conversation
+    public string ReceiverId { get; set; } // The ID of the other user in the conversation
+    public string ReceiverUsername { get; set; } // The username of the other user
 }
