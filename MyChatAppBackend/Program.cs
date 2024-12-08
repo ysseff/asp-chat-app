8bc9b278a163e2d0a8d5027d4c944926f5f31422
Youssef Mahmoud <139473311+ysseff@users.noreply.github.com>
2024-12-08 17:49:21 +0200

    registered CORS in the dependencies

--- MyChatAppBackend/Program.cs
+++ MyChatAppBackend/Program.cs
@@ -11,6 +11,16 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed(origin => true);
+    });
+});
 
 builder.Services.AddDependencies(builder.Configuration);
 // Add more services similarly
@@ -22,9 +32,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
     app.MapOpenApi();
 }
+app.UseCors();
 
 app.UseHttpsRedirection();
-
 
 app.UseAuthentication();
 
@@ -33,6 +43,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapHub<ChatHub>("/chathub");
-
 
 app.Run();
